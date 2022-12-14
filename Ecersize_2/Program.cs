a5e7c044ffa894c5de44c7a6079c1619d24840fa
Valentina <valush31@gmail.com>
2022-12-14 19:17:11 +0300

    add exercize 1

---
+++ Ecersize_2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт,
+// какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+Console.WriteLine("Введите два числа:");
+
+string first =  Console.ReadLine();
+string second =  Console.ReadLine();
+ 
+int number1 = int.Parse(first);
+int number2 = int.Parse(second);
+ 
+    if (number1 > number2)
+  
+    {
+ 
+        Console.WriteLine($"{0} наибольшее число", number1);
+    }
+        
+    else
+ 
+    {
+ 
+        Console.WriteLine("{0} наибольшее число ", number2);
+ 
+    }
+ 
+Console.ReadLine();
